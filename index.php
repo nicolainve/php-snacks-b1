1ecef14b7d2aa4539efc480436db24799fa216d8
Nicola <nginvernizzi1@gmail.com>
2020-12-15 16:24:18 +0100

    input controls

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>php-snacks-b1</title>
+    <script src="https://cdn.jsdelivr.net/npm/vue@2.6.12/dist/vue.js"></script>
 
 </head>
 
@@ -12,30 +13,40 @@
     <?php
 
     if (empty($_GET['name']) || empty($_GET['mail']) || empty($_GET['age'])) {
-        $response = 'inserisci';
+        $response = 'Inserisci i dati';
     } else {
         if (strlen($_GET['name']) > 3 && !is_numeric($_GET['name'])) {
 
             if (strpos($_GET['mail'], '@') || strpos($_GET['mail'], '.')) {
 
                 if (is_numeric($_GET['age'])) {
-                    $response = 'accesso';
+                    $response = 'Accesso';
                 } else {
-                    $response = 'eta non valida';
+                    $response = 'Età non valida';
                 }
             } else {
-                $response = 'mail non valida';
+                $response = 'Mail non valida';
             }
         } else {
-            $response = 'nome breve o non consentito';
+            $response = 'Nome breve o non consentito';
         }
     }
 
     ?>
 
-    <a href="./?name=Mario&mail=mariorossi@mail.com&age=30">Completa</a>
+    <div id="app">
+        <input type="text" v-model="name" placeholder="name">
+        <input type="text" v-model="mail" placeholder="mail">
+        <input type="text" v-model="age" placeholder="age">
+
+        <a :href="'./?name=' + name + '&mail=' + mail + '&age=' + age">Inserisci</a>
+    </div>
+
 
     <p><?php echo $response; ?></p>
+
+    <!-- JS -->
+    <script src="main.js"></script>
     
 </body>
 </html>
