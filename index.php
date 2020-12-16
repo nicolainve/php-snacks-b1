e85065e495c5955e7ca0322b08c7fd6914859dd4
Nicola <nginvernizzi1@gmail.com>
2020-12-16 10:09:15 +0100

    correction

--- index.php
+++ index.php
@@ -14,22 +14,18 @@
 
     if (empty($_GET['name']) || empty($_GET['mail']) || empty($_GET['age'])) {
         $response = 'Inserisci i dati';
-    } else {
-        if (strlen($_GET['name']) > 3 && !is_numeric($_GET['name'])) {
-
-            if (strpos($_GET['mail'], '@') || strpos($_GET['mail'], '.')) {
-
-                if (is_numeric($_GET['age'])) {
-                    $response = 'Accesso';
-                } else {
-                    $response = 'Età non valida';
-                }
-            } else {
-                $response = 'Mail non valida';
-            }
-        } else {
-            $response = 'Nome breve o non consentito';
-        }
+    }
+    else if (strlen($_GET['name']) <= 3) {
+        $response = 'Nome breve o non consentito';
+    }
+    else if (strpos($_GET['mail'], '@') || strpos($_GET['mail'], '.')) {
+        $response = 'Mail non valida';
+    }
+    else if (!is_numeric($_GET['age'])) {
+        $response = 'Età non valida';
+    }
+    else {
+        $response = 'Accesso riuscito';
     }
 
     ?>
